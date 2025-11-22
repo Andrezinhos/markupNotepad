d5d3a51da8b64bbca7f22476929ebc8d7210ed54
Andre Magon <andremiguel2magon@gmail.com>
2025-11-22 14:23:08 -0300

    added tabs and shows the names of the current open file

--- editor.py
+++ editor.py
@@ -1,20 +1,23 @@
-from PySide6.QtWidgets import QMainWindow, QTextEdit, QFileDialog
-from PySide6.QtGui import QAction, QFont
+from PySide6.QtWidgets import QMainWindow, QTextEdit, QFileDialog, QTabWidget
+from PySide6.QtGui import QAction, QFont, QIcon
 
 class Editor(QMainWindow):
-
-    
     def __init__(self):
-        APP_VERSION = "0.2.1"
+        APP_VERSION = "0.4.0"
         super().__init__()
         self.setWindowTitle(f"Markup {APP_VERSION}")
+        self.setWindowIcon(QIcon("favicon.ico"))
         self.resize(800, 600)
         font = QFont("Arial", 12)
 
-        self.textArea = QTextEdit()
-        self.textArea.setFont(font)
-        self.setCentralWidget(self.textArea)
+        # Tabs
+        self.tabs = QTabWidget()
+        self.setCentralWidget(self.tabs)
+        self.addNewTab(font)   
+        self.tabs.addTab(QTextEdit(), "+")
+        self.tabs.currentChanged.connect(self.checkNewTab)
 
+        # Menu bar
         menu = self.menuBar()
         archive = menu.addMenu("Arquivo")
         format = menu.addMenu("Formatar")
@@ -36,8 +39,8 @@ class Editor(QMainWindow):
 
         open.triggered.connect(self.OpenFile)
         save.triggered.connect(self.SaveFile)
-        majorFont.triggered.connect(lambda: self.textArea.setFont(QFont("Arial", 14)))
-        minorFont.triggered.connect(lambda: self.textArea.setFont(QFont("Arial", 12)))
+        majorFont.triggered.connect(lambda: self.tabs.currentWidget().setFont(QFont("Arial", 14)))
+        minorFont.triggered.connect(lambda: self.tabs.currentWidget().setFont(QFont("Arial", 12)))
         exit.triggered.connect(self.close)
         
     def OpenFile(self):
@@ -45,11 +48,29 @@ class Editor(QMainWindow):
         if way:
             with open (way, "r", encoding="utf-8") as f:
                 content = f.read()
-                self.textArea.setPlainText(content)
+                editor = self.tabs.currentWidget()
+                editor.setPlainText(content)
+                fileName = way.split("/")[-1]
+                self.tabs.setTabText(self.tabs.currentIndex(), fileName)
 
     def SaveFile(self):
         way, _ = QFileDialog.getSaveFileName(self, "Save File", "", "Text Files (*.txt);;All Files (*)")
         if way:
+            editor = self.tabs.currentWidget()
             with open (way, "w", encoding="utf-8") as f:
-                content = self.textArea.toPlainText()
+                content = editor.toPlainText()
                 f.write(content)
+                fileName = way.split("/")[-1]
+                self.tabs.setTabText(self.tabs.currentIndex(), fileName)
+    
+    def addNewTab(self, font, title="New Note"):
+        textArea = QTextEdit()
+        textArea.setFont(font)
+        index = self.tabs.count() - 1
+        self.tabs.insertTab(index, textArea, title)
+        self.tabs.setCurrentIndex(index)
+
+    def checkNewTab(self, index):
+        if self.tabs.tabText(index) == "+":
+            font = QFont("Arial", 12)
+            self.addNewTab(font, "New Note")
