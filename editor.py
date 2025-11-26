80f7e221126d3ad48c3c597b63b67a54e9c5c86a
Andre Magon <andremiguel2magon@gmail.com>
2025-11-26 09:49:02 -0300

    saves direct from keyboard shortcut

--- editor.py
+++ editor.py
@@ -1,10 +1,10 @@
-from PySide6.QtWidgets import QMainWindow, QFileDialog
+from PySide6.QtWidgets import QMainWindow, QFileDialog, QPushButton
 from PySide6.QtGui import QAction, QFont, QIcon
 from tabs import Tabs
 
 class Editor(QMainWindow):
     def __init__(self):
-        APP_VERSION = "0.5.6"
+        APP_VERSION = "0.7.4"
         super().__init__()
         self.setWindowTitle(f"Markup {APP_VERSION}")
         self.setWindowIcon(QIcon("favicon.ico"))
@@ -13,20 +13,26 @@ class Editor(QMainWindow):
         # Tabs
         self.tabs = Tabs()
         self.setCentralWidget(self.tabs)
+        self.currentFile = None
+        self.filePaths = {}
+        self.isDirty = False
+        self.dirtyTabs = set()
 
         # Menu bar
         menu = self.menuBar()
-        archive = menu.addMenu("Archive")
+        files = menu.addMenu("Files")
         format = menu.addMenu("Format")
 
         open = QAction("Open File", self)
-        save = QAction("Save File", self)
+        save = QAction("Save", self)
+        saveAs = QAction("Save As", self)
         exit = QAction("Exit", self)
 
-        archive.addAction(open)
-        archive.addAction(save)
-        archive.addSeparator()
-        archive.addAction(exit)
+        files.addAction(open)
+        files.addAction(save)
+        files.addAction(saveAs)
+        files.addSeparator()
+        files.addAction(exit)
 
         majorFont = QAction("Increase Font", self)
         minorFont = QAction("Decrease Font", self)
@@ -34,10 +40,15 @@ class Editor(QMainWindow):
         format.addAction(majorFont)
         format.addAction(minorFont)
 
+        open.setShortcut("Ctrl+O")        
         open.triggered.connect(self.OpenFile)
+        save.setShortcut("Ctrl+S")
         save.triggered.connect(self.SaveFile)
+        saveAs.setShortcut("Ctrl+Shift+S")
+        saveAs.triggered.connect(self.SaveFileAs)
         majorFont.triggered.connect(lambda: self.tabs.currentWidget().setFont(QFont("Arial", 14)))
         minorFont.triggered.connect(lambda: self.tabs.currentWidget().setFont(QFont("Arial", 12)))
+        exit.setShortcut("Ctrl+Q")
         exit.triggered.connect(self.close)
         
     def OpenFile(self):
@@ -47,11 +58,25 @@ class Editor(QMainWindow):
                 content = f.read()
                 editor = self.tabs.currentWidget()
                 editor.setPlainText(content)
+                editor.textChanged.connect(self.markDirty)
                 fileName = way.split("/")[-1]
+
                 self.tabs.setTabText(self.tabs.currentIndex(), fileName)
+                self.filePaths[self.tabs.currentIndex()] = way
 
     def SaveFile(self):
-        way, _ = QFileDialog.getSaveFileName(self, "Save File", "", "Text Files (*.txt);;All Files (*)")
+        index = self.tabs.currentIndex()
+        if index in self.filePaths:
+            path = self.filePaths[index]
+            editor = self.tabs.currentWidget()
+            with open (path, "w", encoding="utf-8") as f:
+                f.write(editor.toPlainText())
+                self.clearDirty(index)
+        else:
+            self.SaveFileAs()
+
+    def SaveFileAs(self):
+        way, _ = QFileDialog.getSaveFileName(self, "Save As", "", "Text Files (*.txt);;All Files (*)")
         if way:
             editor = self.tabs.currentWidget()
             with open (way, "w", encoding="utf-8") as f:
@@ -59,4 +84,19 @@ class Editor(QMainWindow):
                 f.write(content)
                 fileName = way.split("/")[-1]
                 self.tabs.setTabText(self.tabs.currentIndex(), fileName)
+                self.filePaths[self.tabs.currentIndex()] = way
+
+    def markDirty(self):
+        #self.isDirty = True
+        index = self.tabs.currentIndex()
+        self.dirtyTabs.add(index)
+        title = self.tabs.tabText(index)
+        if not title.endswith("*"):
+            self.tabs.setTabText(index, title + "*")
+            
+    def clearDirty(self, index):
+        if index in self.dirtyTabs:
+            self.dirtyTabs.remove(index)
+        title = self.tabs.tabText(index).rstrip("*")
+        self.tabs.setTabText(index, title)
     
